a22cc7bc1b6024304648180d9de5d03234ce76e6
Aleksandr Ivantsov <7022713@gmail.com>
2023-08-25 15:21:57 +0300

    Adds swagger docs for Users endpoints

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,7 +1,10 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const { User } = require("../../models/user");
 const { HttpError, ctrlWrapper } = require("../../utils");
+
+const { SECRET_KEY } = process.env;
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -15,11 +18,23 @@ const register = async (req, res) => {
   const hashPassword = await bcrypt.hash(password, salt);
 
   const result = await User.create({ ...req.body, password: hashPassword });
+
+  const payload = {
+    id: result._id,
+  };
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+  await User.findByIdAndUpdate(result._id, { token });
+
   res.status(201).json({
     user: {
       name: result.name,
       email: result.email,
+      birthday: result.birthday,
+      phone: result.phone,
+      skype: result.skype,
+      avatarURL: result.avatarURL,
     },
+    token,
   });
 };
 
--- models/user.js
+++ models/user.js
@@ -28,6 +28,22 @@ const userSchema = new Schema(
       type: String,
       default: null,
     },
+    birthday: {
+      type: String,
+      default: null,
+    },
+    phone: {
+      type: String,
+      default: null,
+    },
+    skype: {
+      type: String,
+      default: null,
+    },
+    avatarURL: {
+      type: String,
+      default: null,
+    },
   },
   { versionKey: false, timestamps: true }
 );
